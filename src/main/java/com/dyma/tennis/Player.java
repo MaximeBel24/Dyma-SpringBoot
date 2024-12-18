d47727e2ad43d1bdfc0f804a4b5182bc63a5498b
Maxime <maxime.b2494@gmail.com>
2024-12-18 23:08:16 +0100

    feature: couche web gestion des erreurs

--- src/main/java/com/dyma/tennis/Player.java
+++ src/main/java/com/dyma/tennis/Player.java
@@ -2,6 +2,7 @@ package com.dyma.tennis;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
@@ -9,6 +10,6 @@ import java.time.LocalDate;
 public record Player(
         @NotBlank(message = "First name is mandatory") String firstName,
         @NotBlank(message = "Last name is mandatory") String lastName,
-        @PastOrPresent(message = "Birth date must be past or present") LocalDate birthDate,
+        @NotNull(message = "Birth date is mandatory") @PastOrPresent(message = "Birth date must be past or present") LocalDate birthDate,
         @Valid Rank rank) {
 }
